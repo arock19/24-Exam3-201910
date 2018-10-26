47e25826aa788c7be1558da8e27fd79be3f20519
arock19 <guptaa5@rose-hulman.edu>
2018-10-25 21:38:00 -0400

    exam problem 1

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Exam 3, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Achintya Gupta.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # TO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -85,8 +85,12 @@ def problem1(numbers):
     Type hints:
       :type numbers:  [int]
     """
+    sm=0
+    for k in range(len(numbers)//3,2*len(numbers)//3):
+        sm=sm+numbers[k]
+    return sm
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # TO: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
